0e9e7f1e9a14008ad3e814e09d40221eb974fda1
abu <aboobacker@hamon.in>
2018-12-17 10:51:30 +0530

    Use environment variable for host

--- hamon/postgres/start.sh
+++ hamon/postgres/start.sh
@@ -1,4 +1,6 @@
 
 echo "listen_addresses = '*'" >> /etc/postgresql/9.3/main/postgresql.conf
+sed -i "88i\host    all      $USER          $HOST          md5" /etc/postgresql/9.3/main/pg_hba.conf
+
 service postgresql restart
 tail -f /var/log/postgresql/postgresql-9.3-main.log
