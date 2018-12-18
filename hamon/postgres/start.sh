f4bc5cd51df89c0a8a3aed4910b812bb32338e4b
abu <aboobacker@hamon.in>
2018-12-18 16:04:35 +0530

    Modify postgres container to accept connections from rails

--- hamon/postgres/start.sh
+++ hamon/postgres/start.sh
@@ -1,6 +1,6 @@
 
 echo "listen_addresses = '*'" >> /etc/postgresql/9.3/main/postgresql.conf
-sed -i "88i\host    all      $USER          $HOST          md5" /etc/postgresql/9.3/main/pg_hba.conf
+sed -i "88i\host    all      $USER          0.0.0.0/0          trust" /etc/postgresql/9.3/main/pg_hba.conf
 
 service postgresql restart
 tail -f /var/log/postgresql/postgresql-9.3-main.log
