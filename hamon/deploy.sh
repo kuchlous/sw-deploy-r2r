0485679b45e5d668a8562e1ec0e9badf903daaee
Ubuntu <ubuntu@ip-172-31-20-30.eu-west-1.compute.internal>
2018-12-28 11:35:23 +0000

    Label current system as nginx

--- hamon/deploy.sh
+++ hamon/deploy.sh
@@ -5,10 +5,14 @@ labels=("elastic=true" "postgres=true" "sw_app=true" "couch=true")
 
 sudo docker swarm init
 
+# label current node as nginx
+sudo docker node update --label-add "nginx=true" $(hostname)
+
 # Get the token to join the swarm as worker and construct the joining command
 # This will be run in all the other machines 
 join_command="sudo docker swarm join --token $(sudo docker swarm join-token --quiet worker) ${NGINX}:2377"
 
+# Add all other machines as swarm workers and assign appropriate labels 
 for ((index=0;index<4;++index)); do
      ssh -i ${SSH_KEY} -o StrictHostKeyChecking=no ${USER}@${nodes[$index]} ${join_command} 
      sudo docker node  update --label-add ${labels[$index]} $(ssh -i ${SSH_KEY} -o StrictHostKeyChecking=no ${USER}@${nodes[$index]} hostname)
