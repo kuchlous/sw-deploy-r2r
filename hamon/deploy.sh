e9c7d2208d96504b9a2b02b600a34c40d9add8e8
abu <abukilayil@gmail.com>
2018-12-31 12:52:55 +0530

    Add some logs

--- hamon/deploy.sh
+++ hamon/deploy.sh
@@ -12,22 +12,35 @@ nodes=(${ELASTIC} ${POSTGRES} ${SW_APP} ${COUCH})
 labels=("elasticsearch=true" "postgres=true" "app=true" "couchbase=true")
 
 sudo docker swarm init
+echo "LOG: ${NGINX} This system initiated swarm cluster and role as master"
 
 # label current node as nginx
 sudo docker node update --label-add "nginx=true" $(hostname)
+echo "LOG: Labeled master node  as nginx=true"
 
 # Get the token to join the swarm as worker and construct the joining command
 # This will be run in all the other machines 
 join_command="sudo docker swarm join --token $(sudo docker swarm join-token --quiet worker) ${NGINX}:2377"
+echo "LOG: join commad is ${join_command}"
 
 # Add all other machines as swarm workers and assign appropriate labels 
 for ((index=0;index<4;++index)); do
-     ssh -i ${SSH_KEY} -o StrictHostKeyChecking=no ${USER}@${nodes[$index]} ${join_command} 
-     sudo docker node  update --label-add ${labels[$index]} $(ssh -i ${SSH_KEY} -o StrictHostKeyChecking=no ${USER}@${nodes[$index]} hostname)
+
+    echo "LOG: Going to ssh to ${nodes[$index]} and set it as a worker"
+    ssh -i ${SSH_KEY} -o StrictHostKeyChecking=no ${USER}@${nodes[$index]} ${join_command}
+    echo "LOG: ${node[$index]} joined to cluster as worker"
+     
+    echo "LOG: Going to label ${node[$index]} as ${labels[$index]}"
+    sudo docker node  update --label-add ${labels[$index]} $(ssh -i ${SSH_KEY} -o StrictHostKeyChecking=no ${USER}@${nodes[$index]} hostname)
+    echo "LOG: ${node[$index]} node labeled as ${labels[$index]}"
 done
 
 # Create shared directory in app host
+echo "LOG: Going to create shared directory in ${SW_APP}"
 ssh -i ${SSH_KEY} -o StrictHostKeyChecking=no ${USER}@${SW_APP} "mkdir -p ${SHARED_DIR}log ${SHARED_DIR}system ${SHARED_DIR}tmp"
+echo "LOG: Created ${SHARED_DIR} in ${SW_APP} "
 
 # Deploy
+echo "LOG: Going to DEPLOY........wait a while"
 sudo docker stack deploy -c docker-compose.yml story
+echo "LOG: Go to the ${NGINX} with your browser "
