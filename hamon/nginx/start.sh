4049c52207d7e8332c394f98d7df7566660045af
Suroor Hussain <suroorhussain@hamon.in>
2019-01-07 18:23:21 +0530

    Run the log in background
    Since 'tail -f' won't exit, the while loop would never execute and
    nginx will stay stopped if it fails

--- hamon/nginx/start.sh
+++ hamon/nginx/start.sh
@@ -5,7 +5,7 @@ openssl req -new -newkey rsa:4096 -days 365 -nodes -x509 \
     -keyout /self-signed.key  -out /self-signed.cert
 sed -i "10i\  server_name $HOST_IP" /etc/nginx/sites-available/spp.conf
 service nginx restart
-tail -f  /var/log/nginx/access.log
+tail -f  /var/log/nginx/access.log&
 while true
 do
     service nginx status
