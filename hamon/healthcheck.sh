5a7a4c6772066f35e291406caf1ec92351135795
abu <aboobacker@hamon.in>
2019-01-17 11:02:12 +0530

    Add some more logs

--- hamon/healthcheck.sh
+++ hamon/healthcheck.sh
@@ -6,26 +6,29 @@ LOG_FILE=deploy.log
 # Get the container id of nginx
 while [ -z $CONTAINER_ID ]; do
     CONTAINER_ID=$(sudo docker ps -qf name=story_sw-nginx)
+    echo "LOG: Nginx container id is : $CONTAINER_ID" >> $LOG_FILE 
     sleep 10
 done
 
 #Try to restart nginx service
 RETURN_MSG=$(sudo docker exec -t  $CONTAINER_ID service nginx restart)
+echo "LOG: Return message of nginx service restart is : $RETURN_MSG" | tee -a $LOG_FILE
 
 #check wether have DNS issue if have redeploy the stack
 if [[ "$RETURN_MSG" =~ "host not found in upstream" ]]; then
         
     # Remove existing story stack
-    echo "Going to remove existing stack" | tee -a $LOG_FILE   
-    sudo docker stack rm story
+    echo "LOG: Going to remove existing stack" | tee -a $LOG_FILE   
+    sudo docker stack rm story | tee -a $LOG_FILE 
 
     # Wait for a while for remove stack
-    echo "Removing....wait...a..while"
+    echo "LOG: Removing....wait...a..while"
     sleep 60
 
 
     # Redeploy the stack
     echo "Going to Redeploy" | tee -a $LOG_FILE   
-    sudo docker stack deploy -c docker-compose.yml story
+    sudo docker stack deploy -c docker-compose.yml story | tee -a $LOG_FILE 
     sleep 60
+    
 fi
